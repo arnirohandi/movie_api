2c235261670a19066ab8e765f44f0a8537c90035
arnirohandi <164883142+arnirohandi@users.noreply.github.com>
2024-07-12 17:24:50 +0200

    Create the Express code to route all of the endpoints

--- index.js
+++ index.js
@@ -34,6 +34,56 @@ app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
 
+
+// New Routes
+
+// Endpoint to get a single movie by title
+app.get('/movies/title/:title', (req, res) => {
+  const { title } = req.params;
+  res.send(`Return data about the movie titled: ${title}`);
+});
+
+// Endpoint to get a genre by name/title
+app.get('/genres/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Return data about the genre: ${name}`);
+});
+
+// Endpoint to get a director by name
+app.get('/directors/:name', (req, res) => {
+  const { name } = req.params;
+  res.send(`Return data about the director: ${name}`);
+});
+
+// Endpoint to register a new user
+app.post('/users', (req, res) => {
+  res.send('Register a new user');
+});
+
+// Endpoint to update user info
+app.put('/users/:username', (req, res) => {
+  const { username } = req.params;
+  res.send(`Update user info for username: ${username}`);
+});
+
+// Endpoint to add a movie to user's favorites
+app.post('/users/:username/favorites/:movieId', (req, res) => {
+  const { username, movieId } = req.params;
+  res.send(`Add movie with ID ${movieId} to favorites for user: ${username}`);
+});
+
+// Endpoint to remove a movie from user's favorites
+app.delete('/users/:username/favorites/:movieId', (req, res) => {
+  const { username, movieId } = req.params;
+  res.send(`Remove movie with ID ${movieId} from favorites for user: ${username}`);
+});
+
+// Endpoint to deregister a user
+app.delete('/users/:username', (req, res) => {
+  const { username } = req.params;
+  res.send(`Deregister user with username: ${username}`);
+});
+
 // Handle error in express
 app.use((err, req, res, next) => {
   console.error(err.stack);
